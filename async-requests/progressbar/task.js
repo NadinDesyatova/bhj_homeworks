e1926d38a9d0a0d6598b2d05a17d2e258926a0e5
NadinDesyatova <nadindinina@yandex.ru>
2024-11-20 15:03:48 +0300

    Task 3 has been adjusted

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -8,22 +8,8 @@ form.addEventListener('submit', (e) => {
 
   const xhr = new XMLHttpRequest();
 
-  xhr.addEventListener('readystatechange', () => {
-    if (xhr.readyState === 1) {
-      progress.value = 0.25;
-    }
-
-    if (xhr.readyState === 2) {
-      progress.value = 0.5;
-    }
-
-    if (xhr.readyState === 3) {
-      progress.value = 0.75;
-    }
-
-    if (xhr.readyState === 4) {
-      progress.value = 1;
-    }
+  xhr.upload.addEventListener("progress", (event) => {
+    progress.value = event.loaded / event.total;
   });
 
   xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload', true);
