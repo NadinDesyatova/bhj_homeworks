6f2757d790a7b803ce3ca784367712acc0d8bd53
NadinDesyatova <nadindinina@yandex.ru>
2024-11-20 16:06:25 +0300

    Task 2 has been adjusted

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -3,51 +3,59 @@ const pollTitle = document.getElementById('poll__title');
 const pollAnswers = document.getElementById('poll__answers');
 const poll = document.querySelector('.poll');
 
-xhr1.addEventListener('load', () => {
-  let wholeResponse = xhr1.response;
-  let pollId = wholeResponse['id'];
-  let data = wholeResponse['data'];
+function sendXhr(xhr, method, url, typeOfResponse, argumentForSend = null, headers = []) {
+  xhr.open(method, url, true);
+  
+  headers.forEach(header => xhr.setRequestHeader(header[0], header[1]));
+
+  xhr.responseType = typeOfResponse;
+
+  xhr.send(argumentForSend);
+};
+
+const xhr = new XMLHttpRequest();
+
+sendXhr(xhr, 'GET', 'https://students.netoservices.ru/nestjs-backend/poll', 'json');
+
+xhr.addEventListener('load', () => {
+  const wholeResponse = xhr.response;
+  const pollId = wholeResponse['id'];
+  const data = wholeResponse['data'];
   pollTitle.textContent = data['title'];
-  let arrayAnswers = data['answers'];
+  const arrayAnswers = data['answers'];
   for (let i = 0; i < arrayAnswers.length; i++) {
-    pollAnswers.insertAdjacentHTML('beforeEnd', '<button class="poll__answer"></button>');
-    document.querySelectorAll('.poll__answer')[i].textContent = arrayAnswers[i];
-    let argumentForSend = 'vote=' + pollId + '&answer='+ i;
-    document.querySelectorAll('.poll__answer')[i].addEventListener('click', (e) => {
+    pollAnswers.insertAdjacentHTML('beforeEnd', '<button class="poll__answer"></button> ');
+    const answer = document.querySelectorAll('.poll__answer')[i];
+    answer.textContent = arrayAnswers[i];
+    const argumentForSend = 'vote=' + pollId + '&answer='+ i;
+    answer.addEventListener('click', (e) => {
       alert('Спасибо, ваш голос засчитан!');
 
       e.preventDefault();
 
-      const xhr2 = new XMLHttpRequest();
+      sendXhr(
+        xhr, 
+        'POST', 
+        'https://students.netoservices.ru/nestjs-backend/poll', 
+        'json', 
+        argumentForSend, 
+        [['Content-type', 'application/x-www-form-urlencoded']]
+      );
 
-      xhr2.addEventListener('load', () => {
+      xhr.addEventListener('load', () => {
         pollAnswers.remove();
-        let wholeResponse2 = xhr2.response;
-        let statistics = wholeResponse2['stat'];
-        let votesNumber = +statistics.reduce((accum, item) => accum + item['votes'], 0);
+        const wholeResponse2 = xhr.response;
+        const statistics = wholeResponse2['stat'];
+        const votesNumber = +statistics.reduce((accum, item) => accum + item['votes'], 0);
         for (let i = 0; i < statistics.length; i++) {
           poll.insertAdjacentHTML('beforeEnd', '<div class="poll__title"></div>');
           poll.querySelectorAll('.poll__title')[i + 1].textContent = statistics[i]['answer'];
-          let votesNumberOfAnswer = ' ' + ((statistics[i]['votes'] / votesNumber) * 100).toFixed(2) + '%';
-          let pollTitleCurrent = poll.querySelectorAll('.poll__title')[i + 1];
+          const votesNumberOfAnswer = ' ' + ((statistics[i]['votes'] / votesNumber) * 100).toFixed(2) + '%';
+          const pollTitleCurrent = poll.querySelectorAll('.poll__title')[i + 1];
           pollTitleCurrent.insertAdjacentHTML('beforeEnd', '<b></b>');
           pollTitleCurrent.querySelector('b').textContent = votesNumberOfAnswer;
         }
       });
-
-      xhr2.open('POST', 'https://students.netoservices.ru/nestjs-backend/poll', true);
-
-      xhr2.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-
-      xhr2.responseType = 'json';
-
-      xhr2.send(argumentForSend);
     });
   }
 });
-
-xhr1.open('GET', 'https://students.netoservices.ru/nestjs-backend/poll', true);
-
-xhr1.responseType = 'json';
-
-xhr1.send();
