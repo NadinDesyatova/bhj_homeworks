060ac4aa2e2fbe8c5eb7bfbee506d92936a6022e
NadinDesyatova <nadindinina@yandex.ru>
2024-11-21 18:27:40 +0300

    Task 2 has been corrected

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,17 +1,13 @@
 const subscribeModal = document.getElementById('subscribe-modal');
 const modalClose = document.querySelector('.modal__close');
 
-subscribeModal.classList.add('modal_active');
+const value = "; " + document.cookie;
+const cookiesValue = value.split('; popup=');
+if (cookiesValue.length !== 2) {
+  subscribeModal.classList.add('modal_active');
+}
 
 modalClose.onclick = () => {
   subscribeModal.classList.remove('modal_active');
   document.cookie = encodeURIComponent('popup') + '=' + encodeURIComponent('closed');
 }
-
-(() => {
-  const value = "; " + document.cookie;
-  const cookiesValue = value.split('; popup=');
-  if (cookiesValue.length === 2) {
-    subscribeModal.classList.remove('modal_active');
-  }
-})();
